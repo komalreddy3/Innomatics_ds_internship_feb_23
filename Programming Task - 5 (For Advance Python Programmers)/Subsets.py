6c7ad0cb942052c864e14dc98bb9c5e029ae3642
KomalKReddy <82363361+komalreddy3@users.noreply.github.com>
2023-03-02 21:00:22 +0530

    Create Subsets.py

---
+++ Programming Task - 5 (For Advance Python Programmers)/Subsets.py
@@ -0,0 +1,36 @@
+"""
+Given an integer array nums of unique elements, return all possible 
+subsets
+ (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+"""
+
+class Solution(object):
+    def subsets(self, nums):
+        ret = []
+        self.dfs(nums, [], ret)
+        return ret
+    
+    def dfs(self, nums, path, ret):
+        ret.append(path)
+        for i in range(len(nums)):
+            self.dfs(nums[i+1:], path+[nums[i]], ret)
